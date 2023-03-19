2f77ed9aa11eed48359550fe1d163e97a00a021d
Mayra Zamora <46287961+mayraazamora@users.noreply.github.com>
2023-03-19 00:51:50 -0500

    deleted debug.log

--- Assets/Scripts/ObjectThrower.cs
+++ Assets/Scripts/ObjectThrower.cs
@@ -18,13 +18,11 @@ public class ObjectThrower : MonoBehaviour
         // Check if the hands trigger button has been pressed
         if (Input.GetButtonDown(triggerName))
         {
-            Debug.Log($"{triggerName} was pressed");
         }
 
         // Check if the hands trigger button has been released
         if (Input.GetButtonUp(triggerName))
         {
-            Debug.Log($"{triggerName} was released");
         }
 
     }
