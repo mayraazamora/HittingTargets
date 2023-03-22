0f0cb6808303dc408b064dfcd385314b1a6651d5
Mayra Zamora <46287961+mayraazamora@users.noreply.github.com>
2023-03-21 22:33:28 -0500

    Game manager script

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public float range;
-    public float speed;
-    private Vector3 initialPosition;
+    public float speed; 
+    private Vector3 initialPosition; // The initial position of the target
 
     private void Start()
     {
@@ -13,11 +13,14 @@ public class Target : MonoBehaviour
 
     void Update()
     {
+        // Glides target side to side while taking into acount it's initial position 
         transform.position = initialPosition + transform.right * Mathf.Sin(Time.time * speed) * range;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log($"target has been hit {collision.collider.gameObject.name}");
+        Destroy(gameObject); // Destroys target bullsye 
+        Destroy(collision.collider.gameObject);  // Destroys food prefabs when colliding with target
+        Debug.Log($"target has been hit {collision.collider.gameObject.name}"); // Logs name of food that touches target 
     }
 }
