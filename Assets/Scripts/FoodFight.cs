05bf4b401770a2638ab1e34f5eb41ae5d2c90577
Mayra Zamora <46287961+mayraazamora@users.noreply.github.com>
2023-03-22 21:28:35 -0500

    Spawns the new target
    Let the target know about the game script. We are passing in the script we are in right now

--- Assets/Scripts/FoodFight.cs
+++ Assets/Scripts/FoodFight.cs
@@ -1,21 +1,26 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class FoodFight : MonoBehaviour
+public class FoodFight : MonoBehaviour // This script is running on that game object we made in the scene
 {
     public int score;
-    public Target targetPrefab;
-
+    public Target targetPrefab; // Target prefab we made in unity 
+    private void Start()
+    {
+        SpawnTarget(); // Spawn the 1st target
+    }
     public void OnTargetHit()
     {
-        score += 1;
-        SpawnTarget();
+        score += 1;  // Increase the score by 1 
+        SpawnTarget(); // Spawn a new target
     }
-
     private void SpawnTarget()
     {
-        Target newTarget = Instantiate(targetPrefab, Vector3.zero, Quaternion.identity);
+        Target newTarget = Instantiate(targetPrefab, Vector3.zero, Quaternion.Euler(-90,0,0)); // Spawns the new target
+        newTarget.game = this;
     }
 }
+// Let the target know about the game script
+// This has now way of telling me when it's been hit 
+// So all you have to do is 
+// The value we want to assign this game variable in our target is actually the script we are in right now
+// The value we're tryign to give this is that script running on that Game (empty object)
+// We are trying to pass in that script we are in right now
--- Assets/Scripts/ObjectThrower.cs
+++ Assets/Scripts/ObjectThrower.cs
@@ -1,16 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class ObjectThrower : MonoBehaviour
 {
-
     public string triggerName;  // Controller input name 
     public Rigidbody[] objects; // Food array being randomly selected 
     public float throwImpulse;  // Throwing food force
-
     private Rigidbody heldObject;  // Food that is chosen at random, cloned, and then spawned
-
     void Update()
     {    
         if (Input.GetButtonDown(triggerName)) // Check if the hands trigger button has been pressed
@@ -20,14 +14,12 @@ public class ObjectThrower : MonoBehaviour
             heldObject.useGravity = false; // Turn off gravity so that food does not fall from your hand as it spawns          
             heldObject.isKinematic = true; // Avoid having external objects interfere with our food while we are holding it
         }
-
         if (Input.GetButtonUp(triggerName)) // Check if the hands trigger button has been released
         {
             heldObject.transform.SetParent(null); // Unparent food from the hand; otherwise, it will follow the hand even on the ground.
             heldObject.useGravity = true; // Restore gravity; otherwise, food will float in space.
             heldObject.isKinematic = false; // We want our food to interact "normally" with external forces of objects.
             heldObject.AddForce(transform.forward * throwImpulse); // Apply a force to the food object to throw it
-
         }
-    }
+    } 
 }
