949f24bb4bbea1543dca0fff556f9899bb812f9d
Mayra Zamora <46287961+mayraazamora@users.noreply.github.com>
2023-03-24 01:30:03 -0500

    Spawn area bounding box is trigger

--- Assets/Scripts/FoodFight.cs
+++ Assets/Scripts/FoodFight.cs
@@ -3,6 +3,7 @@ public class FoodFight : MonoBehaviour // This script is running on that game ob
 {
     public int score;
     public Target targetPrefab; // Target prefab we made in unity 
+    public BoxCollider spawnArea; 
     private void Start()
     {
         SpawnTarget(); // Spawn the 1st target
@@ -14,6 +15,11 @@ public class FoodFight : MonoBehaviour // This script is running on that game ob
     }
     private void SpawnTarget()
     {
+        Vector3 randomPosition = new Vector3(
+            Random.Range(spawnArea.bounds.min.x, spawnArea.bounds.max.x),
+            Random.Range(spawnArea.bounds.min.y, spawnArea.bounds.max.y),
+            Random.Range(spawnArea.bounds.min.z, spawnArea.bounds.max.z));
+           
         Target newTarget = Instantiate(targetPrefab, Vector3.zero, Quaternion.Euler(-90,0,0)); // Spawns the new target
         newTarget.game = this; // Let the target know about the game script // The value we want to assign this game variable in our target is actually the script we are in right now
     }
