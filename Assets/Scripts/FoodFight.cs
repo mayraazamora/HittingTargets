207e8a9d2c1f5b142f0db21067933515ead95486
Mayra Zamora <46287961+mayraazamora@users.noreply.github.com>
2023-03-23 23:58:14 -0500

    Update FoodFight.cs

--- Assets/Scripts/FoodFight.cs
+++ Assets/Scripts/FoodFight.cs
@@ -15,8 +15,6 @@ public class FoodFight : MonoBehaviour // This script is running on that game ob
     private void SpawnTarget()
     {
         Target newTarget = Instantiate(targetPrefab, Vector3.zero, Quaternion.Euler(-90,0,0)); // Spawns the new target
-        newTarget.game = this; // Let the target know about the game script
+        newTarget.game = this; // Let the target know about the game script // The value we want to assign this game variable in our target is actually the script we are in right now
     }
 }
-// The value we want to assign this game variable in our target is actually the script we are in right now
-// The value we're tryign to give this is that script running on that Game (empty object)
